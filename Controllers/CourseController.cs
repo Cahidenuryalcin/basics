abaf0a42244377a19d7150311b7e861fd2b245ac
Cahidenuryalcin <cnuryal@gmail.com>
2024-11-22 22:12:56 +0300

    end

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -5,12 +5,7 @@ namespace basics.Controllers;
 
 //course
 public class CourseController: Controller{
-    //course index
-
-
-    // public string Index(){
-    //     return "course/index";     
-    // }
+   
     public IActionResult Index(){
         var kurs= new Course();
         kurs.Id =1;
@@ -20,29 +15,22 @@ public class CourseController: Controller{
         return View(kurs);
         
     }
-    // //course list
-    // public IActionResult List(){
-    //     return View(); // view ile kullanildiginda ayni isimde dosyayi arar
-    // }
+   
 
 
-public IActionResult Details(){
-        var kurs= new Course();
-        kurs.Id =1;
-        kurs.Title= "Aspnet core";
-        kurs.Description= "Yeni basladim guzel gibi";
-        kurs.Image="1.jpg";
+public IActionResult Details(int? id){
+        if (id==null)
+        {
+            // return Redirect("/course/list");
+            return RedirectToAction("List", "Course");
+
+        }
+        var kurs= Repository.GetById(id);
         return View(kurs);
         
     }
     //course list
     public IActionResult List(){
-        // var kurslar= new List<Course>(){
-        //     new Course(){Id=1,Title="aspnet kursu", Description="guzel bir kurs", Image="1.jpg"},
-        //     new Course(){Id=2,Title="pyhton kursu", Description="iyi bir kurs", Image="2.jpg"},
-        //     new Course(){Id=3,Title="javascript kursu", Description=" harika bir kurs", Image="3.jpg"},
-        // };
-        // return View("CourseList", kurslar); // farkli isimde değer gönderilmek istenilirse
         
          return View("CourseList", Repository.Courses); 
     }
